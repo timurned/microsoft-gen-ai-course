c4e7a9d0ee2dab44d6110e23367d2beddfdc69f4
timur343 <60493548+timur343@users.noreply.github.com>
2025-06-10 21:08:01 +0000

    lesson 9 assignments

--- 09-building-image-applications/python/oai-app.py
+++ 09-building-image-applications/python/oai-app.py
@@ -14,7 +14,7 @@ try:
     # Create an image by using the image generation API
     generation_response = client.images.generate(
         model="dall-e-3",
-        prompt='Bunny on horse, holding a lollipop, on a foggy meadow where it grows daffodils',    # Enter your prompt text here
+        prompt='Bunny on horse, holding a book, on a sunny meadow where it grows roses',    # Enter your prompt text here        
         size='1024x1024',
         n=1
     )
